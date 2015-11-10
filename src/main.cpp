7b3a3d82a2b09d45765f0e0a246832ecfb808756
Vili Väinölä <vili.vainola@aalto.fi>
2015-11-10 16:23:57 +0200

    Mainin sanakirja muutettu wstringiksi

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,22 @@
 
 int main(void)
 {
-	// get word list from file
-	std::ifstream file;
-	std::string line;
-	std::set<std::string> words;
-	file.open("sanat.txt");
-	while (std::getline(file, line)){
-		words.insert(line);
-	}
-	file.close();
-
-	GUI gui(960, 640, words);
-	gui.run();
-	return 0;
+    setlocale(LC_ALL, "");
+    // get word list from file
+    std::ifstream file;
+    std::string line;
+    std::set<std::wstring> words;
+    file.open("sanat.txt");
+    while (std::getline(file, line)){
+        words.insert(boost::locale::conv::utf_to_utf<wchar_t>(line.c_str(),line.c_str() + line.size()));
+    }
+    file.close();
+    //auto t = sj::create_matrix(4,5,L"homohomokäkihomohomo");
+    //auto k = sj::find_words(t,words);
+    //for (auto i : k){
+    //    std::wcout << i.w_word() << std::endl;
+    //}
+    //GUI gui(960, 640, words);
+    //gui.run();
+    return 0;
 }
