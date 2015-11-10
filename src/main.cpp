ea9afb51178766138deed48264c5821b32d5f909
Liukkonen Janne <liukkoj4@niksula.hut.fi>
2015-11-10 23:55:07 +0200

    Viimeisin. static_cast ei toimi aina, atoi() voisi.

--- src/main.cpp
+++ src/main.cpp
@@ -19,13 +19,13 @@ int main(int argc, char **argv)
                 break;
             case 'w':
                 filename = optarg;
-                std::cout << "filename: " << filename << std::endl;
+                std::wcout << L"filename: " << sj::utf8_to_wstring(filename) << std::endl; // Jotain hämärää, hävittää ääkköset. Testaa ./main -c 1 ja sen jälkeen ./main -c 1 -w sanat.txt
                 break;
             case 'm':
-                M = static_cast<int>(optarg);
+                M = atoi(optarg); // Täytyy kattoa toimiiko char * kanssa, vai vaatiiko \0:n
                 break;
             case 'n':
-                N = static_cast<int>(optarg);
+                N = atoi(optarg);
                 break;
             case '?':
                 if (optopt == 'c')
@@ -53,6 +53,7 @@ int main(int argc, char **argv)
        /// fprintf(stderr, "TODO, not yet implemented");
         (void) M;
         (void) N;// TODO
+	std::wcout << L"Printing words with matrix: " << std::endl << matrix_as_string << std::endl;;
         auto char_matrix = sj::create_matrix(M, N, matrix_as_string);
         std::vector<sj::Path> solved_words = sj::find_words(char_matrix, words);
         for (auto i : solved_words) {
@@ -69,7 +70,8 @@ int main(int argc, char **argv)
     //for (auto i : k){
     //    std::wcout << i.w_word() << std::endl;
     //}
-    GUI gui(960, 640, words);
-    gui.run();
+//
+//    GUI gui(960, 640, words);
+ //   gui.run();
     return 0;
 }
