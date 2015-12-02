fd2c7957324f9ba2bfa9a2be3b91c34742d21c4e
Hirvola Tatu <hirvolt2@lyta.org.aalto.fi>
2015-12-02 19:29:00 +0200

    Poistettu turhia rivejä testien makefilestä ja selvennetty kommenteilla joitakin kohtia.

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,5 +1,9 @@
 #include <gtest/gtest.h> //GoogleTest libraries
 
+/*
+ * Main function that calls the tests
+ * Returns 0 if all tests are passed and 1 if some or all tests didn't pass
+ */
 GTEST_API_ int main(int argc, char **argv){
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
