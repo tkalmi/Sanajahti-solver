0d61c0eb7ca98d3959da40eed83f56b20c4b1506
Janne Liukkonen <janne.liukkonen@aalto.fi>
2015-12-09 12:45:05 +0200

    Poistettu o:n argumentit, luotu -O flagi argumenttia varten. -O test.png käyttää test.png:ää OCR:ssa

--- src/main.cpp
+++ src/main.cpp
@@ -13,37 +13,35 @@
 
 bool s_interrupt = false;
 
-static void handler(int signal)
-{
+static void handler(int signal) {
     switch (signal) {
         case SIGINT:
             fprintf(stderr, "Caught interrupt signal \n");
             s_interrupt = true;
-	    exit(1);
-     	    break;
-	case SIGILL:
-	    fprintf(stderr, "Illegal instruction\n");
-	    break;
-	case SIGABRT:
-	    fprintf(stderr, "Caught SIGABRT: usually caused by and abort() or assert()\n");
-	    break;
-	case SIGFPE:
-	    fprintf(stderr, "Caught SIGFPE: arithmetic exception\n");
-	    break;
-	case SIGSEGV:
-	    fprintf(stderr, "Caught SIGSEGV meaning segmentation fault. Perhaps GPU drivers fails? \n");
-	    exit(1);
-	    break;
-	case SIGTERM:
-	default:
-	   fprintf(stderr, "SIGTERM: termination request was sent to this program. Good bye!\n");
-	   exit(1);
-	   break;
+            exit(1);
+            break;
+        case SIGILL:
+            fprintf(stderr, "Illegal instruction\n");
+            break;
+        case SIGABRT:
+            fprintf(stderr, "Caught SIGABRT: usually caused by and abort() or assert()\n");
+            break;
+        case SIGFPE:
+            fprintf(stderr, "Caught SIGFPE: arithmetic exception\n");
+            break;
+        case SIGSEGV:
+            fprintf(stderr, "Caught SIGSEGV meaning segmentation fault. Perhaps GPU drivers fails? \n");
+            exit(1);
+            break;
+        case SIGTERM:
+        default:
+            fprintf(stderr, "SIGTERM: termination request was sent to this program. Good bye!\n");
+            exit(1);
+            break;
     }
 }
 
 struct settings {
-    
     bool text_ui = false; // Default values allowed in C++11.
     bool ocr_on = false;
     bool android_input = false;
@@ -51,38 +49,37 @@ struct settings {
     int event_num = 3;
     int M = 4;
     int N = 4;
-    
+
     std::string words_filename = "sanat.txt"; //default wordlist. Changeable by command flags.
     std::string ocr_filename = "scrot.png"; // default image file (PNG) to be used with OCR-library.
     std::string matrix_as_string;
 };
 
-struct settings parse_settings(int argc, char **argv)
-{
+struct settings parse_settings(int argc, char **argv) {
     settings opt;
     int options;
     std::string str; //Helper variable for option -m
     bool loytyy_x = false; // Helper variable with matrix validity check.
-    
+
     opterr = 0; // getopt() parameters
-    
-    while ((options = getopt(argc, argv, "ac:e:m:o:pw:")) != -1) {
-        switch(options) {
-	    case 'a': // Android support
-		opt.android_input = true;
-		break;
+
+    while ((options = getopt(argc, argv, "ac:e:m:oO:pw:")) != -1) {
+        switch (options) {
+            case 'a': // Android support
+                opt.android_input = true;
+                break;
             case 'c': // Disabling GUI, printing to shell.
-		if (opt.ocr_on == false)
+                if (opt.ocr_on == false)
                     opt.matrix_as_string = optarg;
-		opt.text_ui = true;
+                opt.text_ui = true;
                 break;
             case 'e':
-            	opt.event_num = atoi(optarg);
-            	std::cout << "event number: " << optarg << std::endl;
-            	break;
+                opt.event_num = atoi(optarg);
+                std::cout << "event number: " << optarg << std::endl;
+                break;
             case 'm': // Matrix input, input as MxN with the 'x' between numbers.
                 if (optarg[0] == '0' || optarg[0] == 'x') {
-                    fprintf(stderr,"Option -m input format is: MxN, for example 4x4\n");
+                    fprintf(stderr, "Option -m input format is: MxN, for example 4x4\n");
                     exit(1);
                 }
                 for (unsigned int i = 0; i < strlen(optarg); i++) {
@@ -92,80 +89,77 @@ struct settings parse_settings(int argc, char **argv)
                             exit(1);
                         }
                         loytyy_x = true;
-                        opt.M = std::stoi (str);
+                        opt.M = std::stoi(str);
                         str.erase();
                         continue;
                     }
                     str.push_back(optarg[i]);
                 }
-                opt.N = std::stoi (str);
+                opt.N = std::stoi(str);
                 if (!loytyy_x) {
                     fprintf(stderr, "Option -m input format is: MxN, for example 4x4\n");
                     exit(1);
                 }
-                fprintf(stderr,"M: %d, N: %d\n", opt.M, opt.N);
+                fprintf(stderr, "M: %d, N: %d\n", opt.M, opt.N);
                 break;
-            case 'o':        	
-            	opt.ocr_on = true;
-                opt.ocr_filename = optarg;
-                try {  // If tesseract fail
+            case 'o':
+                opt.ocr_on = true;
+                try { // If tesseract fails
+                    system("adb shell screencap -p /sdcard/scrot.png && adb pull /sdcard/scrot.png && `adb shell rm /sdcard/scrot.png &>/dev/null`");
                     opt.matrix_as_string = ocr(opt.ocr_filename);
-                } 
-                catch (std::exception e){
+                    if (std::remove(opt.ocr_filename.c_str()) != 0)
+                        std::cout << "Could not remove temporary screenshot file: " << opt.ocr_filename << std::endl;
+                } catch (std::exception e) {
                     exit(1);
-                }  
-		break;
-     	    case 'p': // Printing words paths as well
-		opt.with_paths = true;
-		break;
+                }
+                break;
+            case 'O':
+                opt.ocr_on = true;
+                opt.ocr_filename = optarg;
+                try { // If tesseract fail
+                    opt.matrix_as_string = ocr(opt.ocr_filename);
+                } catch (std::exception e) {
+                    exit(1);
+                }
+                break;
+            case 'p': // Printing words paths as well
+                opt.with_paths = true;
+                break;
             case 'w': // Defining other wordlist to be used.
                 opt.words_filename = optarg;
-                std::cout << "filename: " << opt.words_filename << std::endl; 
+                std::cout << "filename: " << opt.words_filename << std::endl;
                 break;
-           case '?':
-           	if (optopt == 'o') {
-                    opt.ocr_on = true;
-                    try {  // If tesseract fails
-                        system("adb shell screencap -p /sdcard/scrot.png && adb pull /sdcard/scrot.png && `adb shell rm /sdcard/scrot.png &>/dev/null`");
-                        opt.matrix_as_string = ocr(opt.ocr_filename);
-                    } 
-                    catch (std::exception e){
-                        exit(1);
-                    }  
-		}
-           	else if (optopt == 'c' || optopt == 'e' || optopt == 'w') {
-           		fprintf(stderr, "Option -%c requires an argument.\n",optopt); // TODO add here help page reference
-			exit(1);
-			}
-            	else {
-			fprintf(stderr, "Invalid option -%c\n", optopt);
-            		exit(1);
-			}
-		break;
-           default:
+            case '?':
+
+                if (optopt == 'c' || optopt == 'e' || optopt == 'w' || optopt == 'C') {
+                    fprintf(stderr, "Option -%c requires an argument.\n", optopt); // TODO add here help page reference
+                    exit(1);
+                } else {
+                    fprintf(stderr, "Invalid option -%c\n", optopt);
+                    exit(1);
+                }
+                break;
+            default:
                 exit(1);
         }
     }
     return opt;
 }
 
-std::set<std::wstring> read_words_from_file (std::string words_filename)  // get word list from file
-{
+std::set<std::wstring> read_words_from_file(std::string words_filename) { // get word list from file
     std::ifstream file;
     std::string line;
     std::set<std::wstring> words;
     file.open(words_filename);
-    
-    while (std::getline(file, line)){
+
+    while (std::getline(file, line)) {
         words.insert(sj::utf8_to_wstring(line));
     }
     file.close();
     return words;
 }
 
-
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     signal(SIGINT, handler); // CTRL+C
     signal(SIGSEGV, handler);
     signal(SIGTERM, handler);
@@ -173,50 +167,49 @@ int main(int argc, char **argv)
 
     setlocale(LC_ALL, "");
     setlocale(LC_NUMERIC, "en_US.utf8"); // OCR requirement.
-    
+
     struct settings opt = parse_settings(argc, argv);
-    
+
     //std::cout << opt.words_filename;
-    std::set<std::wstring> words = read_words_from_file (opt.words_filename); // Read words from file to memory.
+    std::set<std::wstring> words = read_words_from_file(opt.words_filename); // Read words from file to memory.
 
 
     if (opt.text_ui == true || opt.ocr_on == true) {
-        if (sj::utf8_to_wstring(opt.matrix_as_string).size() != static_cast<unsigned int>(opt.M*opt.N)){
+        if (sj::utf8_to_wstring(opt.matrix_as_string).size() != static_cast<unsigned int> (opt.M * opt.N)) {
             std::cout << "Matrix size does not match dimensions!\n";
             return 1;
         }
         for (unsigned int i = 0; i < sj::utf8_to_wstring(opt.matrix_as_string).size(); i++) {
             std::cout << char_to_str(opt.matrix_as_string, i);
-            if ((i+1) % opt.M == 0)
+            if ((i + 1) % opt.M == 0)
                 std::cout << std::endl;
-            }
-	sj::Solver solver(words, opt.matrix_as_string, opt.M, opt.N);
-        std::vector<sj::Path> solved_words = solver.Paths();
-        std::cout << "Found words:" << std::endl; 
-	for (auto i : solved_words) { 
-	    if(opt.with_paths == true)
-            	std::cout << i.path_str() << std::endl;
-	    else
-		std::cout << i.word() << std::endl;
         }
-	std::cout << "Total words: " << solved_words.size() << std::endl;
-	if (opt.android_input == false)
-		return 1;
+        sj::Solver solver(words, opt.matrix_as_string, opt.M, opt.N);
+        std::vector<sj::Path> solved_words = solver.Paths();
+        std::cout << "Found words:" << std::endl;
+        for (auto i : solved_words) {
+            if (opt.with_paths == true)
+                std::cout << i.path_str() << std::endl;
+            else
+                std::cout << i.word() << std::endl;
+        }
+        std::cout << "Total words: " << solved_words.size() << std::endl;
+        if (opt.android_input == false)
+            return 1;
     }
     if (opt.android_input == true) { // Input solver's results to Android via ADB-connection.
-	if (opt.matrix_as_string.size() == 0)
-		fprintf(stderr,"Nothing to do. Give me matrix!\n");
-	sj::Solver solver(words, opt.matrix_as_string, opt.M, opt.N);
-	std::pair<int,int> res = get_res(opt.ocr_filename);
-	int res_x = res.first;
-	int res_y = res.second;
+        if (opt.matrix_as_string.size() == 0)
+            fprintf(stderr, "Nothing to do. Give me matrix!\n");
+        sj::Solver solver(words, opt.matrix_as_string, opt.M, opt.N);
+        std::pair<int, int> res = get_res(opt.ocr_filename);
+        int res_x = res.first;
+        int res_y = res.second;
         solver.Android_Solve(res_x, res_y, opt.event_num);
-    }    
-    else if (opt.text_ui == false) { // Start GUI.
+    } else if (opt.text_ui == false) { // Start GUI.
         GUI gui(960, 640, words);
         gui.run();
         return 0;
     }
-    
-    return 0; 
+
+    return 0;
 }
